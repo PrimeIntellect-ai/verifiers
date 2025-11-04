15bc2e32a48d3948bba102cf789a756cded1f3dc
Mika Senghaas <mail@mikasenghaas.de>
2025-11-04 15:27:19 +0000

    Make example_id unique in EnvGroup

--- verifiers/envs/env_group.py
+++ verifiers/envs/env_group.py
@@ -1,6 +1,6 @@
 from typing import Mapping
 
-from datasets import concatenate_datasets
+from datasets import Dataset, concatenate_datasets
 from openai import AsyncOpenAI
 
 from verifiers import (
@@ -148,6 +148,27 @@ class EnvGroup(Environment):
             f"Initialized EnvGroup with {len(envs)} environments: {self.env_names}"
         )
 
+    def format_dataset(
+        self,
+        dataset: Dataset,
+        system_prompt: str | None = None,
+        few_shot: list[ChatMessage] | None = None,
+        question_key: str = "question",
+        answer_key: str = "answer",
+    ) -> Dataset:
+        """
+        Forceully creates `example_id` and `prompt` columns if not present.
+        """
+
+        # Remove the example_id column present in the individual env datasets and add global ids
+        if "example_id" in dataset.column_names:
+            dataset = dataset.remove_columns(["example_id"])
+        dataset = dataset.add_column("example_id", range(len(dataset)))  # type: ignore
+
+        assert "example_id" in dataset.column_names
+        assert "prompt" in dataset.column_names
+        return dataset
+
     async def rollout(
         self,
         client: AsyncOpenAI,
@@ -158,7 +179,7 @@ class EnvGroup(Environment):
         state: State | None = None,
         task: str = "default",
         info: Info | None = None,
-        id: int = 0,
+        example_id: int = 0,
         sampling_args: SamplingArgs | None = None,
         **kwargs,
     ) -> tuple[str | list[ChatMessage], State]:
@@ -188,7 +209,7 @@ class EnvGroup(Environment):
             state,
             task,
             info,
-            id,
+            example_id,
             sampling_args,
             **kwargs,
         )
