efbfd71a18ef136937741aedaf070d90bc810279
Kyle Avery <9327972+kyleavery@users.noreply.github.com>
2025-11-09 12:09:43 -0600

    add missing lora parameters

--- verifiers/rl/trainer/config.py
+++ verifiers/rl/trainer/config.py
@@ -298,6 +298,9 @@ class RLConfig(TrainingArguments):
                     lora_alpha=self.lora_alpha,
                     target_modules=self.lora_target_modules,
                     task_type="CAUSAL_LM",
+                    use_rslora=self.lora_use_rslora,
+                    lora_dropout=self.lora_dropout,
+                    modules_to_save=self.lora_modules_to_save,
                 )
 
         self.per_device_train_batch_size = self.micro_batch_size
