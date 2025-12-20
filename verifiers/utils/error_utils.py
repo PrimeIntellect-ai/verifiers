574e9278b1a643d53543411c77fd945bbb646e46
Mika Senghaas <mail@mikasenghaas.de>
2025-12-20 12:31:24 +0000

    fix ty

--- verifiers/utils/error_utils.py
+++ verifiers/utils/error_utils.py
@@ -1,12 +1,12 @@
-from typing import Callable
+from typing import Callable, cast
 
 import verifiers as vf
 
 
 def get_error_chain(
-    error: BaseException, parent_type: type[BaseException] | None = None
+    error: BaseException | None, parent_type: type[BaseException] | None = None
 ) -> list[BaseException]:
-    """Get a causal error chain. If parent_type is not None, the chain will be truncated at the first error that is not a child of parent_type."""
+    """Get a causal error chain. If parent_type is specified, the chain will be truncated at the first error that is not a child of parent_type."""
     error_chain = []
     while error is not None:
         if parent_type is not None and not isinstance(error, parent_type):
@@ -18,7 +18,7 @@ def get_error_chain(
 
 def get_vf_error_chain(error: BaseException) -> list[vf.Error]:
     """Get an error chain containing only vf errors."""
-    return get_error_chain(error, parent_type=vf.Error)
+    return cast(list[vf.Error], get_error_chain(error, parent_type=vf.Error))
 
 
 class ErrorChain:
