b409f53dfd976d7bb8063ecd05cc58ae95e1e221
will brown <williambrown97@gmail.com>
2026-02-08 03:12:22 -0800

    Resolve experimental env exports conflict with SandboxMixin

--- verifiers/envs/experimental/__init__.py
+++ verifiers/envs/experimental/__init__.py
@@ -1,3 +1,4 @@
 from .agent_env import AgentEnv
+from .sandbox_mixin import SandboxMixin
 
-__all__ = ["AgentEnv"]
+__all__ = ["AgentEnv", "SandboxMixin"]
---
+++ verifiers/envs/experimental/sandbox_mixin.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+
+class SandboxMixin:
+    """Experimental mixin placeholder for sandbox-capable environments.
+
+    This class exists to keep merge compatibility while sandbox mixin APIs
+    are being finalized.
+    """
+
+    pass
