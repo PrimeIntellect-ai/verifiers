f7db2bf65bf37efb59c988f610e8b73e3aceccb6
rasdani <73563550+rasdani@users.noreply.github.com>
2026-06-08 22:03:04 +0000

    Add REDSearcher search taskset

--- verifiers/envs/experimental/composable/tasksets/search/__init__.py
+++ verifiers/envs/experimental/composable/tasksets/search/__init__.py
@@ -3,7 +3,13 @@
 from .search_tasksets import (
     make_openseeker_taskset,
     make_quest_taskset,
+    make_redsearcher_taskset,
     make_search_taskset,
 )
 
-__all__ = ["make_openseeker_taskset", "make_quest_taskset", "make_search_taskset"]
+__all__ = [
+    "make_openseeker_taskset",
+    "make_quest_taskset",
+    "make_redsearcher_taskset",
+    "make_search_taskset",
+]
