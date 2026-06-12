3a368438c696ae7631b284e0e0da9b5f904e0f2b
Xeophon <46377542+xeophon@users.noreply.github.com>
2026-06-12 10:54:45 +0200

    Bump harnesses to 0.1.3

--- packages/harnesses/harnesses/__init__.py
+++ packages/harnesses/harnesses/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .mini_swe_agent import MiniSWEAgent, MiniSWEAgentConfig, MiniSWEAgentProgramConfig
 from .opencode import OpenCode, OpenCodeConfig, OpenCodeProgramConfig
