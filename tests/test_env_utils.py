b4837a9ac64f097bb23a84a2b279aa46a7a97df4
Xeophon <46377542+xeophon@users.noreply.github.com>
2026-07-07 21:40:31 +0200

    Delete tests/v1/test_eval_compat.py, tests/v1/test_legacy_wire_task.py, tests/test_env_utils.py, tests/test_init_script.py

--- tests/test_env_utils.py
+++
@@ -1,6 +0,0 @@
-from verifiers.utils.env_utils import env_module_name
-
-
-def test_env_module_name_normalizes_hub_refs():
-    assert env_module_name("primeintellect/reverse-text") == "reverse_text"
-    assert env_module_name("primeintellect/reverse-text@v1") == "reverse_text"
