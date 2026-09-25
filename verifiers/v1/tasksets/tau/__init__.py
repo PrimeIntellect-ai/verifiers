62957fe162f8c3f10277c31f9f66ce450bf0fcad
hallerite <git@hallerite.com>
2026-09-25 14:41:18 -0700

    Add shared adapter for official Tau runners

---
+++ verifiers/v1/tasksets/tau/__init__.py
@@ -0,0 +1,13 @@
+from verifiers.v1.tasksets.tau.env import TauEnv, TauEnvConfig
+from verifiers.v1.tasksets.tau.harness import TauHarness
+from verifiers.v1.tasksets.tau.taskset import TauConfig, TauData, TauTask, TauTaskset
+
+__all__ = [
+    "TauConfig",
+    "TauData",
+    "TauEnv",
+    "TauEnvConfig",
+    "TauHarness",
+    "TauTask",
+    "TauTaskset",
+]
